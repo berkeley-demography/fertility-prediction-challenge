b0b704ca51ae67c84024cd85e97d771c3b25abe7
dennis feehan <feehan@berkeley.edu>
2024-05-12 14:55:35 -0700

    fixed model filename

--- submission.R
+++ submission.R
@@ -29,7 +29,7 @@ clean_df <- function(df, background_df = NULL){
 }
 
 
-predict_outcomes <- function(df, background_df = NULL, model_path = "./model.rds"){
+predict_outcomes <- function(df, background_df = NULL, model_path = "./best_final_fit.rds"){
   # Generate predictions using the saved model and the input dataframe.
   
   # The predict_outcomes function accepts a dataframe as an argument
